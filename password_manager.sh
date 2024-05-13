e9a73f26792992286416d1672a0528db5f0aa3fb
haruki1227041209 <haruki122704@gmail.com>
2024-05-13 13:48:19 +0900

    update service_data

--- password_manager.sh
+++ password_manager.sh
@@ -9,9 +9,7 @@ read user_name
 echo "パスワードを入力してください："
 read password
 
-echo "サービス名:$service_nameユーザー名:$user_nameパスワード:$password" >> service_data.sh
+echo "サービス名:$service_name ユーザー名:$user_name パスワード:$password" >> service_data.sh
 
-echo "サービス名：$service_name"
-echo "ユーザー名：$user_name"
-echo "パスワード：$password"
 echo "Thank you!"
+
