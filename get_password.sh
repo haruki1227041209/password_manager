eecdd045526a20ff3008a34a633f3a0c238c2125
haruki1227041209 <haruki122704@gmail.com>
2024-05-13 22:56:05 +0900

    Add loop-function

--- get_password.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh
-
-echo "サービス名を入力してください："
-read input_service_name
-cat service_data.sh | grep "サービス名:$input_service_name" | awk '{ for(i=1; i<=NF; i++) print $i }'
-
-
--- password_manager.sh
+++ password_manager.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
-echo "パスワードマネージャーへようこそ！"
-echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
-read options
+until [ "$options" = "Exit" ]; do
+	echo "パスワードマネージャーへようこそ！"
+	echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+	read options
 
-if [ "$options" = "Add Password" ]; then
-    echo "サービス名を入力してください："
-    read service_name
-    echo "ユーザー名を入力してください："
-    read user_name
-    echo "パスワードを入力してください："
-	read password
-	echo "サービス名:$service_nameユーザー名:$user_nameパスワード:$password" >> service_data.sh
-	echo "サービス名：$service_name"
-	echo "ユーザー名：$user_name"
-	echo "パスワード：$password"
-	echo "Thank you!"
+	if [ "$options" = "Add Password" ]; then
+		echo "サービス名を入力してください："
+		read service_name
+		echo "ユーザー名を入力してください："
+		read user_name
+		echo "パスワードを入力してください："
+		read password
+		echo "サービス名:$service_name ユーザー名:$user_name パスワード:$password" >> service_data.sh
+		echo "サービス名：$service_name"
+		echo "ユーザー名：$user_name"
+		echo "パスワード：$password"
+		echo "Thank you!"
 
-elif [ "$options" = "Get Password" ]; then
-    echo "サービス名を入力してください："
-	read input_service_name
-	result_service=`grep "サービス名:$input_service_name" service_data.sh`
-	result_service_name=`echo "$result_service" | awk '{print $1}'` 
-	result_user_name=`echo "$result_service" | awk '{print $2}'`
-	result_password=`echo "$result_service" | awk '{print $3}'`
-	if [ "サービス名:$input_service_name" = "$result_service_name" ]; then
- 		echo "サービス名：$result_service_name"
-		echo "ユーザー名：$result_user_name"
-		echo "パスワード：$result_password"
-	else
-		echo "そのサービスは登録されていません。"
+	elif [ "$options" = "Get Password" ]; then
+		echo "サービス名を入力してください："
+		read input_service_name
+		result_service=`grep "サービス名:$input_service_name" service_data.sh`
+		result_service_name=`echo "$result_service" | awk '{print $1}'` 
+		result_user_name=`echo "$result_service" | awk '{print $2}'`
+		result_password=`echo "$result_service" | awk '{print $3}'`
+		if [ "サービス名:$input_service_name" = "$result_service_name" ]; then
+	 		echo "サービス名：$result_service_name"
+			echo "ユーザー名：$result_user_name"
+			echo "パスワード：$result_password"
+		else
+			echo "そのサービスは登録されていません。"
+		fi
+	elif [ "$options" = "Exit" ]; then
+		echo "Thank you!"
 	fi
-fi
+done
