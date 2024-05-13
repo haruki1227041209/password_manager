7381ec4cbc55e6d70e93a839c099a70567f22f7a
haruki1227041209 <haruki122704@gmail.com>
2024-05-13 13:26:05 +0900

    Add feature output

---
+++ password_manager.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+echo "パスワードマネージャーへようこそ！"
+echo "サービス名を入力してください："
+read service_name
+echo "ユーザー名を入力してください："
+read user_name
+echo "パスワードを入力してください："
+read password
+
+echo "サービス名：$service_name"
+echo "ユーザー名：$user_name"
+echo "パスワード：$password"
+echo "Thank you!"
